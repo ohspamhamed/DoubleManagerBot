6c033696b690166ee0933c1f83b9b0626edb75ea
Arian <AllwenWaker81@gmail.com>
2016-08-01 05:04:07 -0700

    Update chat.lua

--- plugins/chat.lua
+++ plugins/chat.lua
@@ -2,7 +2,7 @@ local function do_keyboard_endchat()
     local keyboard = {}
     keyboard.inline_keyboard = {
     	{
-    		{text = '🔚', callback_data = '/end'}
+    		{text = '', callback_data = '/end'}
 	    }
     }
     return keyboard
@@ -17,7 +17,7 @@ if chat_info == 'block' then
  api.sendMessage(msg.chat.id, '`- Sorry !`\n_You are Blocked_', true) 
 else
  db:hset(hash, user_id, 'true')
- api.sendMessage(msg.chat.id, '*- Chat Started*\n `Now all messages will be send to BeatBotTeam Members`\n\n_please send your message now!_', true) 
+ api.sendMessage(msg.chat.id, '`Chat Strated`', true) 
  end
  end
 if blocks[1] == 'end' then
@@ -25,40 +25,40 @@ if chat_info == 'block' or chat_info == 'false' then
 return nil 
 else
  db:hset(hash, user_id, 'false')
-api.sendMessage(msg.chat.id, '*- Chat ended* \n/start `Return to home`', true) 
+api.sendMessage(msg.chat.id, '`Chat Ended`', true) 
 end
 end
 if msg.chat.type == 'private' and chat_info == 'true' then
 if blocks[1] == 'end' or blocks[1] == 'chat' then return nil end
-api.forwardMessage('-119919729', msg.chat.id, msg_id) 
-api.sendKeyboard(msg.chat.id, '`- Sent !`\n_Please wait for reply_'  ,do_keyboard_endchat(), true)
+api.forwardMessage('-1001048657305', msg.chat.id, msg_id) 
+api.sendKeyboard(msg.chat.id, '`Sent!`\n*Wait For Reply*'  ,do_keyboard_endchat(), true)
 end
 if blocks[1] == 'block' then
-if msg.reply and msg.reply.forward_from and msg.chat.type == 'group' and msg.chat.id == -119919729 and not blocks[2] then
+if msg.reply and msg.reply.forward_from and msg.chat.type == 'group' and msg.chat.id == -1001048657305 and not blocks[2] then
 msg = msg.reply
 local user_id = msg.forward_from.id
  db:hset(hash, user_id, 'block')
 api.sendMessage(msg.chat.id, '_User '..user_id..' Blocked_', true) 
 api.sendMessage(user_id, '`- Sorry`\n_You are Blocked_\n\n*Chat ended.*', true) 
 else
- if msg.chat.type == 'group' and msg.chat.id == -119919729 then
+ if msg.chat.type == 'group' and msg.chat.id == -1001048657305 then
  if msg.reply then return nil end
 local user_id = blocks[2]
  db:hset(hash, user_id, 'block')
 api.sendMessage(msg.chat.id, '_User '..user_id..' Blocked_', true) 
-api.sendMessage(user_id, '`- Sorry`\n_You are Blocked_', true) 
+api.sendMessage(user_id, '`You re Blocked`', true) 
 end 
 end
 end
 if blocks[1] == 'unblock' then
-if msg.reply and msg.reply.forward_from and msg.chat.type == 'group' and msg.chat.id == -119919729 and not blocks[2] then
+if msg.reply and msg.reply.forward_from and msg.chat.type == 'group' and msg.chat.id == -1001048657305 and not blocks[2] then
 msg = msg.reply
 local user_id = msg.forward_from.id
  db:hset(hash, user_id, 'false')
 api.sendMessage(msg.chat.id, '_User '..user_id..' UnBlocked_', true) 
 api.sendMessage(user_id, '_You are UnBlocked_', true) 
 else
- if msg.chat.type == 'group' and msg.chat.id == -119919729 then
+ if msg.chat.type == 'group' and msg.chat.id == -1001048657305 then
   if msg.reply then return nil end
 local user_id = blocks[2]
  db:hset(hash, user_id, 'false')
@@ -67,11 +67,11 @@ api.sendMessage(user_id, '_You are UnBlocked_', true)
 end 
 end
 end
-    if msg.reply and msg.reply.forward_from and msg.chat.type == 'group' and msg.chat.id == -119919729 then
+    if msg.reply and msg.reply.forward_from and msg.chat.type == 'group' and msg.chat.id == -1001048657305 then
    msg = msg.reply_to_message
     local receiver = msg.forward_from.id
     local input = blocks[1]
-      api.sendMessage(receiver, '- Reply : \n\n'..input, false)
+      api.sendMessage(receiver, '*Answer :*'..input, false)
 end
 end
 return {
