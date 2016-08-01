d6a6077717e7013694bb823fb1d992cb753d30fb
Arian <AllwenWaker81@gmail.com>
2016-08-02 00:42:22 +0430

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 return {
-	bot_api_key = '252711575:AAHIDNzYIPhKY5LHLIrCsV8FULYQskhhrrE',
+	bot_api_key = '252711575:AAEOf6Jep6KRzIyMw2ioWrpSEeYT0MWvYUw',
 	version = '3.1', -- /aupdate for v3.1
 	testing_mode = false,
 	admin = {
