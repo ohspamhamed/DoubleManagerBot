f0cc8d2bb088cc2005e4215084a389b78ac15985
Arian <AllwenWaker81@gmail.com>
2016-08-04 02:06:32 +0430

    Update startmessage.lua

--- plugins/startmessage.lua
+++ plugins/startmessage.lua
@@ -104,7 +104,7 @@ end
 
 local action = function(msg, blocks, ln)
     -- save stats
-    if blocks[1] == 'start' then
+    if blocks[1] == 'start2' then
         db:hset('bot:users', msg.from.id, 'xx')
         db:hincrby('bot:general', 'users', 1)
         if msg.chat.type == 'private' then
@@ -115,7 +115,7 @@ local action = function(msg, blocks, ln)
         return
     end
     local keyboard = make_keyboard()
-    if blocks[1] == 'help' then
+    if blocks[1] == 'help2' then
         if msg.chat.type == 'private' then
             local message = make_text(lang[ln].help.private, msg.from.first_name:mEscape())
             local keyboard = do_keyboard_private()
