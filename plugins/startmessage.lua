050a742a1c51c73bbbfd68d3a2e7102b79f23fdf
Arian <AllwenWaker81@gmail.com>
2016-08-16 18:20:23 +0430

    Update startmessage.lua

--- plugins/startmessage.lua
+++ plugins/startmessage.lua
@@ -106,34 +106,31 @@ end
 
 local action = function(msg, blocks, ln)
     -- save stats
-if blocks[1] == 'start' or blocks[1] == 'help' then
+    if blocks[1] == 'start' then
         db:hset('bot:users', msg.from.id, 'xx')
         db:hincrby('bot:general', 'users', 1)
         if msg.chat.type == 'private' then
-            local message = [[📍 Salam ]]
+            local message = make_text(lang[ln].help.private, msg.from.first_name:mEscape())
             local keyboard = do_keyboard_private()
             api.sendKeyboard(msg.from.id, message, keyboard, true)
-            end
-        local res = api.sendKeyboard(msg.from.id, 'Select One Of This *Keyboards* :)', keyboard, true)
+        end
+        return
+    end
+    local keyboard = make_keyboard()
+    if blocks[1] == 'help' then
+        if msg.chat.type == 'private' then
+            local message = make_text(lang[ln].help.private, msg.from.first_name:mEscape())
+            local keyboard = do_keyboard_private()
+            api.sendKeyboard(msg.from.id, message, keyboard, true)
+            return
+        end
+        local res = api.sendKeyboard(msg.from.id, 'Choose the *role* to see the available commands:', keyboard, true)
         if res then
             api.sendMessage(msg.chat.id, lang[ln].help.group_success, true)
         else
             api.sendKeyboard(msg.chat.id, lang[ln].help.group_not_success, do_keyboard_startme(), true)
         end
     end
-    if msg.cb then
-        local query = blocks[1]
-        local text
-        if query == 'info_button' then
-            keyboard = do_keybaord_credits()
-		    api.editMessageText(msg.chat.id, msg.message_id, lang[ln].credits, keyboard, true)
-		    return
-		end
-if query == 'home' then
-            local text = [[Salam...]]
-            local keyboard = do_keyboard_private()
-        api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
-end
         local with_mods_lines = true
         if query == 'user' then
             text = lang[ln].help.all
